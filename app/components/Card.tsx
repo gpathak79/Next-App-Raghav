6151dc3960ab9a48f3a5f123d8b537539f410bed
raghav.pathak <raghav.pathak@tothenew.com>
2024-05-28 15:04:09 +0530

    session 2 changes

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,12 +1,15 @@
-// components/Card.tsx
+'use client';
+
 import React from "react";
 import Link from "next/link";
+import Button from "./Button";
+import { useRouter } from "next/navigation";
 
 interface BlogPost {
-    id: number;
+  _id: string;
   title: string;
   author: string;
-  date_published: string;
+  datePublished: string;
   content: string;
 }
 
@@ -15,28 +18,44 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ post }) => {
+
+  const router = useRouter();
+
+  const handleDelete = async (id: string) => {
+    try {
+      await fetch(`/api/posts?id=${id}`, {
+        method: "DELETE",
+      });
+      router.refresh();
+    } catch (error) {
+      console.error('Error deleting post', error);
+    }
+  };
+
   return (
-    <div className="max-w-sm rounded ">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
       <div className="px-6 py-4">
-        <div className="font-bold">{post.title}</div>
-        <p>
+        <div className="font-bold text-xl mb-2">{post.title}</div>
+        <p className="text-gray-700 text-base">
           {post.content.length > 20
             ? post.content.substring(0, 20) + "..."
             : post.content}
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span >
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           {post.author}
         </span>
-        <span >
-          {post.date_published}
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          {post.datePublished}
         </span>
-        <Link href={`/blog/${post.id}`}>
+        <Link href={`/blog/${post._id}`}>
           <p className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mt-2">
             Read More
           </p>
         </Link>
+        <Button className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mt-2" onClick={() => handleDelete(post._id)}>Delete</Button>
+
       </div>
     </div>
   );
